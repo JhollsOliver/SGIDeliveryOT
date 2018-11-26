50edb77efe749605a5820a8a4c099f159fa23974
Giovanne Oliveira <jhollsantos@gmail.com>
2018-11-26 10:31:27 -0200

    [1.0] - First Release

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+    'ApiUrl':"https://deliverybeta.jhollsoliver.me/merchantapp/api",
+	'DialogDefaultTitle':"SGI Delivery",
+	'APIHasKey':"630c1a67f0e84ea24232153e307b3a29",
+	'debug': false
+};
